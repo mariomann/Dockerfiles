3d7930444b9a9a06cacb8d26ced9b481c1cedae0
Banana-Git <_simon.becker@web.de>
2015-06-29 10:39:20 +0200

    modified build_images.sh for usage with jenkins

--- build_images.sh
+++ build_images.sh
@@ -7,18 +7,19 @@ for i in $( ls | grep -e "Dockerfile_*" );
     do
 	# get name of current app-server (needed for tagging the Image)
 	APP_SERVER=${i#*_}
-	echo $APP_SERVER
-	# just for testing, $BUILD_NUMBER will be set by Jenkins during the job later on
-	BUILD_NUMBER=1
-        
-	echo $i
+	echo Building Dockerfile for: $APP_SERVER
+	# just for testing, $BUILD_NUMBER=1. The BUILD_NUMBER environment variabbl will be set by Jenkins during the job later on
+	# BUILD_NUMBER=1
+
+	#echo $i
         cp $i Dockerfile
-        cat Dockerfile
 	
         # build Docker-image
-	echo docker build --tag=sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER .
+	echo Executing Command: docker build --tag=sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER .
+	docker build --tag=sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER .
 	# start Docker-container for current image
-	echo docker run -d -P --name ticket-monster_$APP_SERVER\_v$BUILD_NUMBER sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
+	echo Executing Command: docker run -d -P --name ticket-monster_$APP_SERVER\_v$BUILD_NUMBER sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
+	docker run -d -P --name ticket-monster_$APP_SERVER\_v$BUILD_NUMBER sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
 	# newline just for testing
 	echo -e ""
 done
