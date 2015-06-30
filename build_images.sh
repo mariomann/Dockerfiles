36d7f6e6a842153c4dbe6a5d16e2b38f49a7c44a
Banana-Git <_simon.becker@web.de>
2015-06-30 10:08:59 +0200

    added docker-compose functionality. the build_images.sh script will now create an entry in the compose/docker-compose.yml file for each app-server specfic Dockerfile in the root directory of this Repository

--- build_images.sh
+++ build_images.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+
+# This script builds Docker-images for several Docker-files and creates a docker-compose.yml file for starting a complete dockerized test-environment
+# The test-environment will be set up completly from scratch every time a new Dockerfile is added to the Git Repo
+
+# create fresh docker-compose.yml file, containing just the inspectIT cmr
+cp compose/compose-base.yml compose/docker-compose.yml
+
+# port which wil be mapped to the port 8080 inside the created containers (incremented in loop)
+HOST_HTTP_PORT=6780
+
 # for-loop iterating over all "Dockerfile_<Appserver>" files (e.g. Dockerfile_jboss, Dockerfile_tomcat,...) in the current directory
-# each found app-server secific Dockerfile will be copied to a file called just Dockerfile
-# then a Dockerimage is created for the current Dockerfile
+# each found app-server specific Dockerfile will be copied to a file called just Dockerfile
+# then a Dockerimage is created for the current Dockerfile and the image is added to a docker-compose.yml file
 for i in $( ls | grep -e "Dockerfile_*" );
     do
 	# get name of current app-server (needed for tagging the Image)
@@ -17,8 +27,25 @@ for i in $( ls | grep -e "Dockerfile_*" );
 	echo Executing Command: docker build --tag=sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER .
 	docker build --tag=sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER .
 	# start Docker-container for current image
-	echo Executing Command: docker run -d -P --name ticket-monster_$APP_SERVER\_v$BUILD_NUMBER sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
-	docker run -d -P --name ticket-monster_$APP_SERVER\_v$BUILD_NUMBER sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
+	#echo Executing Command: docker run -d -P --name ticket-monster_$APP_SERVER\_v$BUILD_NUMBER sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
+	#docker run -d -P --name ticket-monster_$APP_SERVER\_v$BUILD_NUMBER sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
+
+	# appending an entry in the docker-compose.yml file for the current image
+        read -d '' COMPOSE_ENTRY <<- EOF
+        $APP_SERVER:
+          image: sbe/ticket-monster_$APP_SERVER:v$BUILD_NUMBER
+          links:
+           - cmr
+          ports:
+           - \"$HOST_HTTP_PORT:8080\"
+          environment:
+           - AGENT_NAME=$APP_SERVER
+        EOF
+
+        echo "$COMPOSE_ENTRY" >> compose/docker-compose.yml
+
+        ((HOST_HTTP_PORT++))
+
 done
 
 # cleanup the created Dockerfile
