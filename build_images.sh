28bd8cd3b105aadfc689b9db2c5f627015d453f5
Banana-Git <_simon.becker@web.de>
2015-07-31 10:49:22 +0200

    agent config is copied in the containers and no longer mounted as volume. Agentname should be set correctly now

--- build_images.sh
+++ build_images.sh
@@ -63,8 +63,6 @@ for i in $( ls | grep Dockerfile_ );
 	   - \"$HOST_HTTP_PORT:8080\"
 	  environment:
 	   - AGENT_NAME=$APP_SERVER
-	  volumes:
-	   - ${WORKSPACE}/inspectIT/agent/config:/opt/agent/active-config
 	EOF
         echo -e "\n$COMPOSE_ENTRY" >> compose/docker-compose.yml
 
