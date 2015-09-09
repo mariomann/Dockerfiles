90b558cc84e66517f94b3b2fe70073a522cd6fa2
Banana-Git <_simon.becker@web.de>
2015-09-09 11:11:18 +0200

    changed naming of created containers. Appending "_jenkins_build" to the container name now

--- start_containers.sh
+++ start_containers.sh
@@ -9,7 +9,7 @@ echo -e "\ngesetzte Variablen:\n BUILD_NUMBER: ${BUILD_NUMBER}\n IMAGE_VERSION: 
 
 # start cmr container
 echo -e "\nStarting CMR-Container"
-docker run -d -p 8182:8182 --name cmr_sc myinspectit/cmr:${IMAGE_VERSION}
+docker run -d -p 8182:8182 --name cmr_jenkins_build myinspectit/cmr:${IMAGE_VERSION}
 
 # LOOP
 # start App Server Containers and run tests
@@ -18,25 +18,25 @@ for IMAGE in $(docker images | grep myinspectit/hello2.*${IMAGE_VERSION} | awk '
 do
 	APP_SERVER=${IMAGE#*_}
 
-	echo -e "\n\nStarting ${APP_SERVER}-Container: docker run -d --name ${APP_SERVER}_sc -p ${HOST_HTTP_PORT}:8080 --link cmr_sc:cmr ${IMAGE}:${IMAGE_VERSION}"
-	docker run -d --name ${APP_SERVER}_sc -p ${HOST_HTTP_PORT}:8080 --link cmr_sc:cmr ${IMAGE}:${IMAGE_VERSION}
+	echo -e "\n\nStarting ${APP_SERVER}-Container: docker run -d --name ${APP_SERVER}_jenkins_build -p ${HOST_HTTP_PORT}:8080 --link cmr_jenkins_build:cmr ${IMAGE}:${IMAGE_VERSION}"
+	docker run -d --name ${APP_SERVER}_jenkins_build -p ${HOST_HTTP_PORT}:8080 --link cmr_jenkins_build:cmr ${IMAGE}:${IMAGE_VERSION}
 
 	echo -e "\nWaiting 45 Seconds for ${APP_SERVER}-Container to start before launching JMeter Tests"
 	sleep 45
 
-	echo -e "\nStarting JMeter Container to execute Tests: docker run --rm --name jmeter_sc -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_sc:${APP_SERVER} --link cmr_sc:cmr -e "APP_SERVER=${APP_SERVER}" sbe/jmeter:run-test-instantly"
-	docker run --rm --name jmeter_sc -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_sc:${APP_SERVER} --link cmr_sc:cmr -e "APP_SERVER=${APP_SERVER}" sbe/jmeter:run-test-instantly
+	echo -e "\nStarting JMeter Container to execute Tests: docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" sbe/jmeter:run-test-instantly"
+	docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" sbe/jmeter:run-test-instantly
 	echo -e "Tests executed"
 	echo -e "\nStopping ${APP_SERVER}-Container"
-	docker stop ${APP_SERVER}_sc
+	docker stop ${APP_SERVER}_jenkins_build
 	echo -e "Removing ${APP_SERVER}-Container"
-	docker rm ${APP_SERVER}_sc
+	docker rm ${APP_SERVER}_jenkins_build
 
 	((HOST_HTTP_PORT++))
 done
 
 # stop and remove cmr container
 echo -e "\nStopping CMR-Container"
-docker stop cmr_sc
+docker stop cmr_jenkins_build
 echo -e "Removing CMR-Container"
-docker rm cmr_sc
+docker rm cmr_jenkins_build
