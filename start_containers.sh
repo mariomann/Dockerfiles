ad4a94f841056146da8d10ee1e92a4cbb6fcbae9
Mario Mann <mann.mario@gmail.com>
2016-02-10 09:24:40 +0100

    Update start_containers.sh

--- start_containers.sh
+++ start_containers.sh
@@ -2,18 +2,18 @@
 
 IMAGE_VERSION=v${BUILD_NUMBER}
 HOST_HTTP_PORT=6780
-
+DOCKER_ID=inspectit-dev
 
 # DEBUG
 echo -e "\ngesetzte Variablen:\n BUILD_NUMBER: ${BUILD_NUMBER}\n IMAGE_VERSION: ${IMAGE_VERSION}\n WORKSPACE: ${WORKSPACE}\n "
 
 # start cmr container
 echo -e "\nStarting CMR-Container"
-docker run -d -p 8182:8182 --name cmr_jenkins_build myinspectit/cmr:${IMAGE_VERSION}
+docker run -d -p 8182:8182 --name cmr_jenkins_build ${DOCKER_ID}/cmr:${IMAGE_VERSION}
 
 # LOOP
 # start App Server Containers and run tests
-for IMAGE in $(docker images | grep myinspectit/hello2.*${IMAGE_VERSION} | awk '{print $1}');
+for IMAGE in $(docker images | grep ${DOCKER_ID}/hello2.*${IMAGE_VERSION} | awk '{print $1}');
 # sample output: myinspectit/hello2_jetty
 do
 	APP_SERVER=${IMAGE#*_}
@@ -24,13 +24,13 @@ do
 	echo -e "\nWaiting 45 Seconds for ${APP_SERVER}-Container to start before launching JMeter Tests"
 	sleep 45
 
-	echo -e "\nStarting JMeter Container to execute Tests: docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" hauptmedia/jmeter bin/jmeter -n -t /jmeter-tests/"
-	docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" hauptmedia/jmeter bin/jmeter -n -t /jmeter-tests/http-requests_${APP_SERVER}.jmx -l /jmeter-tests/results/result_http-requests_${APP_SERVER}.jtl.
+	echo -e "\nStarting JMeter Container to execute Tests: docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" inspectit-dev/jmeter bin/jmeter -n -t /jmeter-tests/"
+	docker run --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" ${DOCKER_ID}/jmeter bin/jmeter -n -t /jmeter-tests/http-requests_${APP_SERVER}.jmx -l /jmeter-tests/results/result_http-requests_${APP_SERVER}.jtl
 	echo -e "Tests executed"
 	echo -e "\nStopping ${APP_SERVER}-Container"
 	docker stop ${APP_SERVER}_jenkins_build
-	echo -e "Removing ${APP_SERVER}-Container"
-	docker rm ${APP_SERVER}_jenkins_build
+	#echo -e "Removing ${APP_SERVER}-Container"
+	#docker rm ${APP_SERVER}_jenkins_build
 
 	((HOST_HTTP_PORT++))
 done
@@ -38,5 +38,5 @@ done
 # stop and remove cmr container
 echo -e "\nStopping CMR-Container"
 docker stop cmr_jenkins_build
-echo -e "Removing CMR-Container"
-docker rm cmr_jenkins_build
+#echo -e "Removing CMR-Container"
+#docker rm cmr_jenkins_build
