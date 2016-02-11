f76a8cb059719ec59e40c6fa6d67d54405fedd41
Mario Mann <mann.mario@gmail.com>
2016-02-11 16:05:22 +0100

    Update start_containers.sh

--- start_containers.sh
+++ start_containers.sh
@@ -21,17 +21,15 @@ do
 	APP_SERVER=${IMAGE#*_}
 
 	echo -e "\n\nStarting ${APP_SERVER}-Container: docker run -d --name ${APP_SERVER}_jenkins_build -p ${HOST_HTTP_PORT}:8080 --link cmr_jenkins_build:cmr ${IMAGE}:${IMAGE_VERSION}"
-	docker run -d --name ${APP_SERVER}_jenkins_build -p ${HOST_HTTP_PORT}:8080 --link cmr_jenkins_build:cmr ${IMAGE}:${IMAGE_VERSION}
+	docker run -d --name ${APP_SERVER}_jenkins_build -p ${HOST_HTTP_PORT}:8080 -h ${APP_SERVER} --link cmr_jenkins_build:cmr ${IMAGE}:${IMAGE_VERSION}
 
 	echo -e "\nWaiting 45 Seconds for ${APP_SERVER}-Container to start before launching JMeter Tests"
 	sleep 45
-
-	#echo -e "\nStarting JMeter Container to execute Tests: docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" inspectit-dev/jmeter bin/jmeter -n -t /jmeter-tests/"
-	#docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link ${APP_SERVER}_jenkins_build:${APP_SERVER} -e "APP_SERVER=${APP_SERVER}" ${DOCKER_ID}/jmeter jmeter -n -t /jmeter-tests/http-requests_${APP_SERVER}.jmx
 	
-	#sleep 1
+	(curl -s http://localhost:${HOST_HTTP_PORT}/hello2/greeting?username=Simon | grep -i Simon >/dev/null ) || exit 1
+	(curl -s http://localhost:${HOST_HTTP_PORT}/hello2/greeting?username=APM | grep -i APM >/dev/null ) || exit 1
+	(curl -s http://localhost:${HOST_HTTP_PORT}/hello2/greeting?username=Novatec | grep -i Novatec >/dev/null ) || exit 1
 	
-	#docker run --rm --name jmeter_jenkins_build -v ${WORKSPACE}/jmeter-tests/:/jmeter-tests/ --link cmr_jenkins_build:cmr -e "APP_SERVER=${APP_SERVER}" ${DOCKER_ID}/jmeter jmeter -n -t /jmeter-tests/restcall_${APP_SERVER}.jmx -l /jmeter-tests/results/result_restcall_${APP_SERVER}.jtl
 	#echo -e "Tests executed"
 	#echo -e "\nStopping ${APP_SERVER}-Container"
 	#docker stop ${APP_SERVER}_jenkins_build
