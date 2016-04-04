b342c4c27ded347f21a457bdb5b1cd22a212593b
Mario Mann <mann.mario@gmail.com>
2016-04-04 15:20:40 +0200

    Update build_images_and_tests.sh

--- build_images_and_tests.sh
+++ build_images_and_tests.sh
@@ -6,34 +6,31 @@ DOCKER_ID=inspectit-dev
 # Furthermore JMeter-tests are created from TEMPLATES
 
 # build inspectIT CMR container
-echo -e "\nBuilding Docker Image for inspectIT CMR"
-docker build --tag=${DOCKER_ID}/cmr:v$BUILD_NUMBER inspectIT/cmr/
+sed -i "s/^RUN wget https:\/\/github\.com.*/ADD inspectit-cmr.linux.x64.tar.gz . \nRUN echo inspectit \\\ /" ./cmr/Dockerfile
+
+# echo -e "\nBuilding Docker Image for inspectIT CMR"
+docker build --tag=${DOCKER_ID}/cmr:v$BUILD_NUMBER ./cmr/
 
 # for-loop iterating over all "Dockerfile_<Appserver>" files (e.g. Dockerfile_jboss, Dockerfile_tomcat,...) in the Jenkins workspace
 # each found app-server specific Dockerfile will be copied to a file called just Dockerfile
 # then a Docker Image tagged with the Jenkins Buildnumber is created for the current Dockerfile
-for i in $( ls | grep Dockerfile_ );
-    do
-	# get name of current app-server (needed for tagging the Image)
-	APP_SERVER=${i#*_}
-	echo -e "\nBuilding Docker Image for $APP_SERVER with integrated inspectIT Agent"
 
-	# copy the app-server specific Dockerfile (e.g. Dockerfile_jboss) to Dockerfile.
-	# necessary, because Docker can only build images from files exactly called "Dockerfile"
-        cp $i Dockerfile
-	
-        # build Docker-image
-	echo Executing Command: docker build --tag=${DOCKER_ID}/hello2_$APP_SERVER:v$BUILD_NUMBER .
-	docker build --tag=${DOCKER_ID}/hello2_$APP_SERVER:v$BUILD_NUMBER .
+for dockerfile in `find . -name Dockerfile -not -path "./cmr*" -type f`;
+do
+
+  # change RUN to ADD built inspectit-agent
+  sed -i "s/^RUN wget https:\/\/github\.com.*/ADD inspectit-agent-sun1.5.zip . \nRUN echo inspectit \\\ /" ${dockerfile}
+
+  DOCKERFILE_PATH=`dirname ${dockerfile}`
+
+  # get name of current app-server (needed for tagging the Image)
+	APP_SERVER=`echo ${DOCKERFILE_PATH} | sed 's/\//_/g'`
+	echo -e "\nBuilding Docker Image for ${APP_SERVER} with new built inspectIT Agent"
+
+  # build Docker-image
+	echo Executing Command: docker build --tag=${DOCKER_ID}/hello2_${APP_SERVER}:v${BUILD_NUMBER} ${DOCKERFILE_PATH}
+	docker build --tag=${DOCKER_ID}/hello2_${APP_SERVER}:v${BUILD_NUMBER} ${DOCKERFILE_PATH}
 
 	# cleanup the created Dockerfile
-	rm Dockerfile
-
-	# create JMeter http-test for each app-server from Template
-        cp jmeter-tests/TEMPLATE_http.jmx jmeter-tests/http-requests_${APP_SERVER}.jmx
-        sed -i -- "s/containerHostname/${APP_SERVER}/g" jmeter-tests/http-requests_${APP_SERVER}.jmx
-        
-	# create JMeter REST-test for each app-server from Template
-        cp jmeter-tests/TEMPLATE_rest.jmx jmeter-tests/restcall_${APP_SERVER}.jmx
-	sed -i -- "s/insertAppServerNameHere/${APP_SERVER}/g" jmeter-tests/restcall_${APP_SERVER}.jmx
+	# rm Dockerfile
 done
